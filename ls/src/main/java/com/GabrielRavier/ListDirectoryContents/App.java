097b4ae6492987f5b4b3b064e0aba4936b21966d
Gabriel Ravier <gabravier@gmail.com>
2022-12-21 17:57:38 +0100

    feat: Managed to set up basic Maven app

---
+++ ls/src/main/java/com/GabrielRavier/ListDirectoryContents/App.java
@@ -0,0 +1,13 @@
+package com.GabrielRavier.ListDirectoryContents;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
